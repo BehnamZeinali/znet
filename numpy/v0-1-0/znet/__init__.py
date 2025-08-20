10156c5329e6665b961ff24afcb2be31f52e2b4a
BehnamZeinali <zeinali.behnam@gmail.com>
2025-08-20 13:14:15 -0400

    Remove

--- numpy/v0-1-0/znet/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import nn
-from . import optim
-from . import autograd
-from ._grad_mode import no_grad, set_grad_enabled, is_grad_enabled
-
--- numpy/v0-1-0/znet/nn/__init__.py
+++
@@ -1,5 +0,0 @@
-from .module import Module
-from .linear import Linear
-from .activation import ReLU
-from .loss import CrossEntropyLoss
-
